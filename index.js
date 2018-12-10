a7b46ddf115a66fde71bee8e52e970b29e1927bc
dev1203 <devsingh120397@gmail.com>
2018-12-09 21:02:49 -0800

    Basic API endpoint and sql

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const ejs = require('ejs')
-const app = express()
+// Express
+const express = require('express');
+
+// Embedded JavaScript
+const ejs = require('ejs');
+
+// Executing express
+const app = express();
+
+// Database connection
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+// Defining public directory for assets
 app.use(express.static('public'));
 
+// Homepage
 app.get('/',function(req,res){
     res.render(__dirname + '/views/index.ejs');
+});
+
+// 404 are redirected to homepage.
+app.get('*',function(req,res){
+    res.redirect('/');
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
